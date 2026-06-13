a3dd73cdad7006d7798752e3ae6ab8aff9cd302b
lianccn <lianmarinheiro2005@gmail.com>
2026-06-13 18:00:58 -0300

    Adicionar métodos de extensão para o arquivo Reservas.cs

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -5,4 +5,37 @@ public class Reserva
     public Sala Sala { get; set; }
     public DateTime Inicio { get; set; }
     public DateTime Fim { get; set; }
+
+    public Reserva() { }
+
+    public Reserva(int salaId, DateTime inicio, DateTime fim)
+    {
+        SalaId = salaId;
+        Inicio = inicio;
+        Fim = fim;
+    }
+}
+
+public static class ReservaExtensions
+{
+    public static double CalcularHorasLivres(this IQueryable<Reserva> reservas, int salaId, DateTime data)
+    {
+        var horasOcupadas = reservas
+            .Where(r => r.SalaId == salaId && r.Inicio.Date == data)
+            .ToList()
+            .Sum(r => (r.Fim - r.Inicio).TotalHours);
+
+        return 11 - horasOcupadas;
+    }
+
+    public static bool HorarioIndisponivel(this IQueryable<Reserva> reservas, int salaId, DateTime inicio, DateTime fim)
+    {
+        return reservas.Any(r => r.SalaId == salaId && inicio < r.Fim && fim > r.Inicio);
+    }
+
+    public static int TotalReunioesUltimosDias(this IQueryable<Reserva> reservas, int dias)
+    {
+        var dataLimite = DateTime.Now.AddDays(-dias);
+        return reservas.Count(r => r.Inicio >= dataLimite);
+    }
 }
